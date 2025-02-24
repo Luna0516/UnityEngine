74e55097c3a5d9eb0c5327243c4e4f8ec850a5cd
Luna0516 <130631559+Luna0516@users.noreply.github.com>
2025-02-24 14:29:25 +0900

    MMO_Unity
    유니티 기초 연습 / 에셋 추가

---
+++ MMO_Unity/Assets/Scripts/Test.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(1.0f, 1.0f, 1.0f));
+    }
+}
